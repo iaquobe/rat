0fd5e9db7a280aabc143b695a54b7fc9d0a279b9
Jakob Lambert-Hartmann <lamberth@in.tum.de>
2023-03-18 01:10:29 +0100

    using u8 as characters and bitvec for codes

--- src/huffman/lib.rs
+++ src/huffman/lib.rs
@@ -1,142 +1,238 @@
-mod huffman {
-    use std::collections::HashMap;
-    use std::collections::BinaryHeap;
-    use std::cmp::Reverse;
+use std::collections::HashMap;
+use std::collections::BinaryHeap;
+use std::cmp::Reverse;
+use bitvec::prelude::*;
 
-    #[derive(Debug,Eq,PartialEq, PartialOrd,Ord)]
-    enum Node {
-        Node{left: Box<Node>, right: Box<Node>},
-        Leaf(char),
+#[derive(Debug,Eq,PartialEq, PartialOrd,Ord)]
+enum Node {
+    Root{left: Box<Node>, right: Box<Node>},
+    Leaf(u8),
+}
+
+#[derive(Debug)]
+pub struct Metadata {
+    character_sequence: String,
+    tree_encoding: String,
+}
+
+use Node::*; 
+
+
+// text -> min heap for char count
+fn frequency(text: &str) -> BinaryHeap<(Reverse<usize>, Node)> {
+    let mut occurence_map: HashMap<u8, usize> = HashMap::new();
+
+    for c in text.bytes() {
+        *occurence_map.entry(c).or_insert(0) += 1;
     }
 
+    // collect into binary heap with peek -> min
+    occurence_map.into_iter()
+        .map(|(c, count)| (Reverse(count), Leaf(c)))
+        .collect()
+}
+
+// compile occurences into tree
+fn huffman_tree(text: &str) -> Node {
+    let mut frequencies = frequency(text); 
+
+    // take 2 lowers and merge, return when only one element left
+    loop {
+        let first = frequencies.pop().expect("heap is empty: input text is empty");
+        let second = frequencies.pop();
+
+        match second {
+            Some((Reverse(count), node)) => frequencies.push(
+                (Reverse(count + first.0.0), Root{ left: (Box::from(first.1)), right: (Box::from(node)) })),
+            None => break first.1,
+        }
+    }
+}
+
+/***
+    returns huffman table (character, length, value)
+    returns huffman table, that is a vector with:
+        character: u8,
+        code: bitvec
+  */
+pub fn huffman_table(text: &str) -> Vec<(u8, BitVec)> {
+    let tree = huffman_tree(text); 
+    let mut table: Vec<(u8, BitVec)> = Vec::new();
+
+    fn explore_branch(table: &mut Vec<(u8, BitVec)>, node: &Node, code: BitVec) {
+        match node {
+            Leaf(c) => {table.push((*c, code));},
+            Root { left, right } => {
+                explore_branch(table, left, {
+                    let mut new_code = code.clone();
+                    new_code.push(false);
+                    new_code
+                }); 
+
+                explore_branch(table, right, {
+                    let mut new_code = code.clone();
+                    new_code.push(true);
+                    new_code
+                });
+            }
+        }
+    }
+    explore_branch(&mut table, &tree, bitvec![]); 
+
+    table
+}
+
+
+pub fn huffman_encoding_metadata(text: &str) -> Metadata {
+    let tree  = huffman_tree(text); 
+
+    let character_sequence: String = huffman_table(text)
+        .iter()
+        .map(|(c,_)| *c as char)
+        .collect();
+
+    let mut tree_encoding = String::new(); 
+    fn explore_branch(node: &Node, tree_encoding: &mut String) {
+        match node {
+            Leaf(_) => {},
+            Root { left, right } => {
+                explore_branch(left, tree_encoding); 
+                tree_encoding.push('0');
+                explore_branch(right, tree_encoding); 
+                tree_encoding.push('1');
+            }
+        }
+    }
+    explore_branch(&tree, &mut tree_encoding); 
     
-    // text -> vec<(char,count)>
-    fn frequency(text: &str) -> Vec<(char, usize)> {
-        let mut occurence_map: HashMap<char, usize> = HashMap::new();
+    Metadata{character_sequence, tree_encoding }
+}
 
-        for c in text.chars() {
-            *occurence_map.entry(c).or_insert(0) += 1;
-        }
+/***
+    take text, and return string wich can be written to file
+  */
+pub fn huffman_encoding(text: &str) -> String {
 
-        let mut result: Vec<(char, usize)> = occurence_map.into_iter()
-            .collect();
+    // get metadata, add character sequence and tree encoding
+    let metadata = huffman_encoding_metadata(text); 
 
-        result
-    }
+    let mut res = String::from(metadata.character_sequence); 
+    res.push_str(&metadata.tree_encoding); 
 
-    // compile occurences into tree
-    fn huffman_tree(text: &str) -> Node {
-        let frequencies = frequency(text); 
 
-        // collect into binary heap with peek -> min
-        let mut heap: BinaryHeap<(Reverse<usize>, Node)> = frequencies.into_iter()
-            .map(|(c, count)| (Reverse(count), Node::Leaf(c)))
-            .collect();
 
-        // take 2 lowers and merge, return when only one element left
+    // encode with tree 
+    let table = huffman_table(text); 
+
+
+    res
+}
+
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn frequency_test_short() {
+        let s = "acab";
+        let mut result   = frequency(s); 
+        let mut expected = BinaryHeap::from([
+                                            (Reverse(2), Leaf(b'a')),
+                                            (Reverse(1), Leaf(b'b')),
+                                            (Reverse(1), Leaf(b'c'))]);
+
         loop {
-            let first = heap.pop().expect("heap is empty: input text is empty");
-            let second = heap.pop();
-
-            match second {
-                Some((Reverse(count), node)) => heap.push((Reverse(count + first.0.0), Node::Node { left: (Box::from(first.1)), right: (Box::from(node)) })),
-                None => break first.1,
+            match (result.pop(),expected.pop()) {
+                (Some(a), Some(b)) => assert_eq!(a, b),
+                (None, None) => break,
+                _ => assert!(false)
             }
         }
     }
 
-    // returns huffman table (character, length, value)
-    fn huffman_table(text: &str) -> Vec<(char, u8, u8)> {
+    #[test]
+    fn frequency_test_long() {
+        let s = "this is an example of a huffman tree";
+        let mut result = frequency(s); 
+        let mut expected = BinaryHeap::from([
+                                            (Reverse(7), Leaf(b' ')),
+                                            (Reverse(4), Leaf(b'a')),
+                                            (Reverse(4), Leaf(b'e')),
+                                            (Reverse(3), Leaf(b'f')),
+                                            (Reverse(2), Leaf(b'h')),
+                                            (Reverse(2), Leaf(b'i')),
+                                            (Reverse(2), Leaf(b'm')),
+                                            (Reverse(2), Leaf(b'n')),
+                                            (Reverse(2), Leaf(b's')),
+                                            (Reverse(2), Leaf(b't')),
+                                            (Reverse(1), Leaf(b'l')),
+                                            (Reverse(1), Leaf(b'o')),
+                                            (Reverse(1), Leaf(b'p')),
+                                            (Reverse(1), Leaf(b'r')),
+                                            (Reverse(1), Leaf(b'u')),
+                                            (Reverse(1), Leaf(b'x'))]);
+
+        loop {
+            match (result.pop(),expected.pop()) {
+                (Some(a), Some(b)) => assert_eq!(a, b),
+                (None, None) => break,
+                _ => assert!(false)
+            }
+        }
+    }
+
+    #[test]
+    fn tree_test_short_1() {
+        let text = "acab";
         let tree = huffman_tree(text); 
-        let mut table: Vec<(char, u8, u8)> = Vec::new();
+        let correct = Root { 
+            left: Box::new(Leaf(b'a')), 
+            right: Box::new(Root {
+                left: Box::new(Leaf(b'c')), 
+                right: Box::new(Leaf(b'b')) }) };
 
-        fn explore_branch(table: &mut Vec<(char, u8, u8)>, node: &Node, length: u8, code: u8) {
-            match node {
-                Node::Leaf(c) => {table.push((*c, length, code));},
-                Node::Node { left, right } => {
-                    explore_branch(table, left, length+1, code << 1); 
-                    explore_branch(table, right, length+1, (code << 1) + 1); 
-                }
-            }
-        }
-        explore_branch(&mut table, &tree, 0, 0); 
-
-        table
+        assert_eq!(tree, correct);
     }
 
-    #[cfg(test)]
-    mod tests {
-        use super::*;
+    #[test]
+    fn tree_test_short_2() {
+        let text = "abcd";
+        let tree = huffman_tree(text); 
+        let correct = Root { 
+            left: Box::new(Root {
+                left: Box::new(Leaf(b'd')), 
+                right: Box::new(Leaf(b'c')) }),
+                right: Box::new(Root {
+                    left: Box::new(Leaf(b'b')), 
+                    right: Box::new(Leaf(b'a')) }) };
 
-        #[test]
-        fn frequency_short() {
-            let s = "acab";
-            let mut r = vec![('a', 2), ('b', 1), ('c',1)]; 
-            let mut f = frequency(s); 
-            r.sort();
-            f.sort();
+        assert_eq!(tree, correct);
+    }
 
-            assert_eq!(f, r);
-        }
+    #[test]
+    fn table_long() { 
+        let text = "this is an example of a huffman tree";
+        let frequencies: HashMap<u8, usize> = frequency(text).into_iter()
+            .filter_map(|(Reverse(count), character)| match character {
+                Node::Leaf(ch) => Some((ch, count)),
+                _ => None
+                
+            })
+            .collect(); 
 
-        #[test]
-        fn frequency_long() {
-            let s = "this is an example of a huffman tree";
-            let mut r = vec![(' ', 7), ('a',4), ('e',4), ('f',3), ('h',2), ('i',2), ('m',2), ('n',2), ('s',2), ('t',2), ('l',1), ('o',1), ('p',1), ('r',1), ('u',1), ('x',1)];
-            let mut f = frequency(s); 
-            f.sort();
-            r.sort();
-
-            assert_eq!(f, r);
-        }
-
-        #[test]
-        fn tree_short_1() {
-            let text = "acab";
-            let tree = huffman_tree(text); 
-            let correct = Node::Node { 
-                left: Box::new(Node::Leaf('a')), 
-                right: Box::new(Node::Node {
-                                left: Box::new(Node::Leaf('c')), 
-                                right: Box::new(Node::Leaf('b')) }) };
-
-            assert_eq!(tree, correct);
-        }
-
-        #[test]
-        fn tree_short_2() {
-            let text = "abcd";
-            let tree = huffman_tree(text); 
-            let correct = Node::Node { 
-                left: Box::new(Node::Node {
-                                left: Box::new(Node::Leaf('d')), 
-                                right: Box::new(Node::Leaf('c')) }),
-                right: Box::new(Node::Node {
-                                left: Box::new(Node::Leaf('b')), 
-                                right: Box::new(Node::Leaf('a')) }) };
-
-            assert_eq!(tree, correct);
-        }
-
-        #[test]
-        fn table_long() { 
-            let s = "this is an example of a huffman tree";
-            let frequencies: HashMap<char, usize> = frequency(s).into_iter()
-                .collect(); 
-
-            // (length_of_code, count_of_char)
-            let mut table: Vec<(u8, usize)> = huffman_table(s).into_iter() 
-                .map(|(character, lenght,_)| (lenght, *frequencies.get(&character).unwrap()))
-                .collect(); 
-            table.sort();
+        // (length_of_code, count_of_char)
+        let mut table: Vec<(usize, usize)> = huffman_table(text).into_iter() 
+            .map(|(character, code)| (code.len(), *frequencies.get(&character).unwrap()))
+            .collect(); 
+        table.sort();
 
 
-            // check that as count decreases, code length increases
-            let mut last_code_len = 0; 
-            for (code_len, _) in table{
-                assert!(last_code_len <= code_len); 
-                last_code_len = code_len; 
-            }
+        // check that as count decreases, code length increases
+        let mut last_code_len = 0; 
+        for (code_len, _) in table{
+            assert!(last_code_len <= code_len); 
+            last_code_len = code_len; 
         }
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,9 @@
-fn main() {
-    println!("Hello, world!");
 
-    let mut v = vec![(1,1),(1,2),(2,2),(2,1),(3,1),(1,3)];
-    v.sort();
-    print!("{:?}", v); 
+
+enum Test {
+    First(char),
+    Second(char),
+}
+
+fn main() {
 }
